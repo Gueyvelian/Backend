14b820ee8cad09984f67f01d82661f6d43343905
lgueyv00 <gueyvelian.lucie@gmail.com>
2025-02-04 10:13:49 +0100

    corection de Projet

--- src/main/java/ssii/entity/Projet.java
+++ src/main/java/ssii/entity/Projet.java
@@ -1,12 +1,19 @@
 package ssii.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
-import lombok.NonNull;
+import lombok.*;
 
 import java.time.LocalDate;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor // lombok, pour générer un constructeur avec les champs @NonNull
+@ToString
 
 public class Projet {
     @Id
